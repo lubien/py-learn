a54db07f293465fd0c6e2ee74fd0f89f3dee2e89
Alan Veloso <alantsv@gmail.com>
2016-11-06 18:10:13 -0300

    CHECKPOINT nov 2016, 6

--- despdom1.py
+++ despdom1.py
@@ -11,6 +11,8 @@ print 'Gastos por pessoa = R$ %s.' % media
 if ana < media:
 	diferenca = media - ana
 	print 'Ana deve pagar: R$ %s' % diferenca
-else:
+elif bia < media:
 	difrenca = media - bia
 	print 'Bia deve pagar: R$ %s' % diferenca
+else:
+	print 'Bia deve pagar: R$ 0.0'
---
+++ somadora1.py
@@ -0,0 +1,10 @@
+# somadora1.py - somadora infinita - versao 1
+
+print 'Digite os valores a somar Seguidos de .'
+print 'Para encerrar digite zero: 0'
+n = float(raw_input(':'))
+total = n 
+while n:
+	n = float(raw_input(':'))
+	total = total + n
+print 'TOTAL: %s' % total
